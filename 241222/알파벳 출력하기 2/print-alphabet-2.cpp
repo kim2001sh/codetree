a418a7a32a97b4e74c76c2fe42c50d62f72c33b0
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-22 19:57:43 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241222/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt = 65;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            
+            if (i > j) {
+                cout << "  ";
+                continue;
+            }
+            
+            cout << (char)cnt << " ";
+            cnt++;
+            if (cnt == 91) cnt = 65; 
+
+        }
+        cout << endl;
+        
+    }
+    return 0;
+}
