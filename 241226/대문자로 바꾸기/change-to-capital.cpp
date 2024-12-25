835ad263a0a62a4d52b6c4c5db56e7344b51f02f
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-26 00:36:15 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241226/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[5][3];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+            cout << char(int(arr[i][j]) - 32) << " ";
+
+        }
+        cout << endl;
+    }
+    return 0;
+}
