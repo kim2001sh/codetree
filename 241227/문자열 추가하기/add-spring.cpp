f2a345af656812efbd71ef92cb7c90b1db180b25
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-27 01:28:53 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    str += "Hello";
+
+    cout << str;
+    return 0;
+}
