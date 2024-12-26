ab5b77b0746204fb6d7cdb9fe106a416624226c1
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-27 00:41:14 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    string str[10];
+    
+    
+
+    for (int i = 0; i < 10; i++) {
+        cin >> str[i];
+        cout << str[i] << endl;
+    }
+
+   
+
+
+    return 0;
+}
