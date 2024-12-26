572306aa45fb39200d83b0938b497d76c608fd49
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-27 00:40:41 +0900

    Title: 문자열 역순으로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/문자열 역순으로 출력하기/print-string-in-reverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str[4];
+
+    for (int i = 0; i < 4; i++) {
+        cin >> str[i];
+    }
+
+    for (int i = 3; i >= 0; i--) {
+        cout << str[i] << endl;
+    }
+
+
+
+    return 0;
+}
