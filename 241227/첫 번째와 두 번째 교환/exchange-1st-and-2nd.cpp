fadf4e4c4b96d906c7bd5380a84290335c0b6eea
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-27 10:55:15 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241227/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+    char x = str[0];
+    char y = str[1];
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == x) str[i] = y;
+        else if (str[i] == y) str[i] = x;
+    }
+    cout << str;
+    return 0;
+}
