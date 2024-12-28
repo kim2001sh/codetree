911730d13f28bdf79f5a86b0857c204c72532210
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-29 00:41:16 +0900

    Title: 알파벳 지우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/알파벳 지우기/remove-alphabet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str1, str2, str3, str4;
+    str3 = "";
+    str4 = "";
+    cin >> str1 >> str2;
+    int x, y;
+
+    for (int i = 0; i < str1.length(); i++) {
+        if (str1[i] >= '0' && str1[i] <= '9') str3 += str1[i];
+        
+    }
+
+    for (int i = 0; i < str2.length(); i++) {
+        if (str2[i] >= '0' && str2[i] <= '9') str4 += str2[i];
+        
+    }
+
+    x = stoi(str3);
+    y = stoi(str4);
+    cout << x + y;
+    return 0;
+}
