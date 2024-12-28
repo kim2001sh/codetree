462b1e1c89e8166f102b8e977534277c7bae9fa6
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-29 02:42:08 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241229/연속부분수열일까/contiguous-array-or-not.cpp
+++ 241229/연속부분수열일까/contiguous-array-or-not.cpp
@@ -7,17 +7,18 @@ int main() {
     int n1, n2;
     string str1, str2;
     cin >> n1 >> n2;
+    cin.ignore(); //이걸 모르면 이 풀이로는 못 푸네...
     bool flag = false;
 
     getline(cin, str1);
-    getline(cin, str2);
+    cin >> str2;
 
     if (str1.length() < str2.length()) {
         cout << "No";
         return 0;
     }
     
-    for (int i = 0; i < n1 - str2.length() + 1; i++) {
+    for (int i = 0; i < str1.length() - str2.length() + 1; i++) {
         if (str1.substr(i, str2.length()) == str2) {
             flag = true;
             break;
