5fcf7b03a4267273e7d7c9df2d6aab4776e6e425
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-29 00:41:37 +0900

    Title: 각 자리 숫자들의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/각 자리 숫자들의 합/sum-of-each-digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int sum = 0;
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] >= '0' && str[i] <= '9') sum += str[i] - '0';
+    }
+
+    cout << sum;
+
+    return 0;
+}
