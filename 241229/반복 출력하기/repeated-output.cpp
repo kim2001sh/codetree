cec29019e66a2eb8102b63e27703eeb94a0e79a9
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-29 18:26:17 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void code(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "12345^&*()_" << endl;
+    }
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int k;
+    cin >> k;
+    code(k);
+    return 0;
+}
