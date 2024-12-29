9a28dbc0b23336c59c26da330c7ab51f673e89c7
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-29 22:52:02 +0900

    Title: 두 정수에 대한 연산값 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void tnwjd(int *a, int *b) {
+    if (*a < *b) {
+        *a += 10;
+        *b *= 2;
+    }
+    else {
+        *b += 10;
+        *a *= 2;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    tnwjd(&a, &b);
+    cout << a << " " << b;
+    return 0;
+}
