22b39dd95dd3ed2bb53e568f995956e21fb6ff72
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-30 18:49:30 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 17ms, Memory: 1MB, Status: Passed - CodeTree

--- 241230/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 241230/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -12,6 +12,11 @@ int main() {
     sort(s2.begin(), s2.end());
     bool flag = true;
 
+    if (s1.length() != s2.length()) {
+        cout << "No";
+        return 0;
+    }
+
     for (int i = 0; i < s1.length(); i++) {
         if (s1[i] != s2[i]) {
             flag = false;
