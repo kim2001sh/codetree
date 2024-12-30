845225d6d56ce9ba2dfbc1942bb949417eda0321
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2024-12-30 10:50:58 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241230/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void star(int n) {
+    if (n == 0) return;
+
+    for (int i = 0; i < n; i++) cout << "* ";
+    cout << endl;
+    star(n - 1);
+    for (int i = 0; i < n; i++) cout << "* ";
+    cout << endl;
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    star(n);
+    return 0;
+}
