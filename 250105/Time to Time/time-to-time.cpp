1ba0a16c29f9a550a876e02c6754d04f9543a97a
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-05 00:00:59 +0900

    Title: Time to Time, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250105/Time to Time/time-to-time.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+    cout << (c - a) * 60 + d - b;
+    return 0;
+}
