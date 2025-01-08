6c0cb7efdc9944533cdab9835f53f1e4d9ecef75
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-09 00:59:04 +0900

    Title: 사각형의 총 넓이 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250109/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[200][200] = {}, x1, x2, y1, y2, cnt = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        for (int x = 0; x < 200; x++) {
+            for (int y = 0; y < 200; y++) {
+                if (x >= x1 && x < x2 && y >= y1 && y < y2) arr[x][y] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < 200; i++) {
+        for (int j = 0; j < 200; j++) {
+            if (arr[i][j] == 1) cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
