9fc440fe1a1a9835cc43d025abd2e5b59f3eb889
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-10 00:49:44 +0900

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250110/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m, k, arr[101], x;
+    bool flag = false;
+
+    cin >> n >> m >> k;
+    for (int i = 0; i < m; i++) {
+        cin >> x;
+        arr[x] += 1;
+        if (arr[x] >= k) {
+            cout << x;
+            flag = true;
+            break;
+        }
+    }
+
+    if (!flag) cout << -1;
+    return 0;
+}
