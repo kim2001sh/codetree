2170ca93414741fa711abb47fed21f9dc3b9349d
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-10 00:42:08 +0900

    Title: 만나는 그 순간, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250110/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m, arra[1001], arrb[1001], t, a = 0, b = 0, total_t = 0;
+    char d;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> d >> t;
+        if (d == 'L') {
+            for (int j = 0; j < t; j++) {
+                arra[j + total_t] = --a;
+            }
+        }
+        else {
+            for (int j = 0; j < t; j++) {
+                arra[j + total_t] = ++a;
+            }
+        }
+        total_t += t;
+
+    }
+    total_t = 0;
+
+    for (int i = 0; i < m; i++) {
+        cin >> d >> t;
+        if (d == 'L') {
+            for (int j = 0; j < t; j++) {
+                arrb[j + total_t] = --b;
+            }
+        }
+        else {
+            for (int j = 0; j < t; j++) {
+                arrb[j + total_t] = ++b;
+            }
+        }
+        total_t += t;
+
+    }
+    bool flag = false;
+    for (int i = 1; i < total_t; i++) {
+        if (arra[i] == arrb[i]) {
+            cout << i + 1;
+            flag = true;
+            break;
+        }
+    }
+
+    if (!flag) cout << -1;
+
+    return 0;
+}
