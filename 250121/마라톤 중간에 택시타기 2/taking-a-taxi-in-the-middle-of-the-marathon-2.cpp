7ff6dfe897289587738bb9154b9dac13ba600f4b
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-21 13:03:07 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -13,22 +13,25 @@ int main() {
 
     }
 
-    for (int i = 1; i < n - 1; i++) {
+    for (int i = 1; i < n - 1; i++) { // i + 1번 체크포인트 무시 
         dis = 0;
         for (int j = 0; j < n; j++) {
             if (j == i) continue;
             else if (j - 1 == i) {
-                dis += arr[j][0] - arr[j - 2][0] >= 0 ? arr[j][0] - arr[j - 2][0] : -1 * (arr[j][0] - arr[j - 2][0]);
-                dis += arr[j][1] - arr[j - 2][1] >= 0 ? arr[j][1] - arr[j - 2][1] : -1 * (arr[j][1] - arr[j - 2][1]);
+                dis += (arr[j][0] - arr[j - 2][0]) >= 0 ? arr[j][0] - arr[j - 2][0] : -1 * (arr[j][0] - arr[j - 2][0]);
+                dis += (arr[j][1] - arr[j - 2][1]) >= 0 ? arr[j][1] - arr[j - 2][1] : -1 * (arr[j][1] - arr[j - 2][1]);
             }
-            else if (i - 1 == j) continue;
+            
+            
             else {
-                dis += arr[j][0] - arr[j - 1][0] >= 0 ? arr[j][0] - arr[j - 1][0] : -1 * (arr[j][0] - arr[j - 1][0]);
-                dis += arr[j][1] - arr[j - 1][1] >= 0 ? arr[j][1] - arr[j - 1][1] : -1 * (arr[j][1] - arr[j - 1][1]);
+                if (j == 0) continue;
+                dis += (arr[j][0] - arr[j - 1][0]) >= 0 ? arr[j][0] - arr[j - 1][0] : -1 * (arr[j][0] - arr[j - 1][0]);
+                dis += (arr[j][1] - arr[j - 1][1]) >= 0 ? arr[j][1] - arr[j - 1][1] : -1 * (arr[j][1] - arr[j - 1][1]);
             }
             
         }
         if (dis < dis_min) dis_min = dis;
+
     }
 
     cout << dis_min;
