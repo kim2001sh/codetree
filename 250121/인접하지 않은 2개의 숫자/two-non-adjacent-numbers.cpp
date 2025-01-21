df174387f3e88146d9d8bf75cf000f420ccafc8c
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-21 13:37:36 +0900

    Title: 인접하지 않은 2개의 숫자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250121/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, max_sum = 0, arr[100];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int i = 0; i < n - 2; i++) {
+        for (j = i + 2; j < n; j++) {
+            if (arr[i] + arr[j] > max_sum) max_sum = arr[i] + arr[j];
+        }
+    }
+
+    cout << max_sum;
+
+    return 0;
+}
