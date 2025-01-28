cc5705f3fd32f0d8679b35f9730c1ce4a8e42072
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-29 04:16:34 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250129/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
+++ 250129/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
@@ -14,8 +14,15 @@ int main() {
         for (int j = 0; j < n; j++) {
             int sum = 0;
             for (int k = 0; k < n; k++) {
-                if (j == k || k == n - 1) continue;
-                sum += abs(arr[k + 1] - arr[k]);
+                if (j == k) continue;
+                if (k - j == -1) {
+                    if (k + 2 >= n) continue;
+                    else sum += abs(arr[k + 2] - arr[k]);
+                }
+                else {
+                    if (k + 1 >= n) continue;
+                    else sum += abs(arr[k + 1] - arr[k]);
+                }
             }
             if (sum < min_value) min_value = sum;
         }
