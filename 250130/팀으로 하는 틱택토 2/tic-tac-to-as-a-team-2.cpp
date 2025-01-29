8f2f92484a2da1f109fdaad1fc39b9c430c4f6fb
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-30 00:59:03 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250130/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 250130/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -5,60 +5,82 @@ using namespace std;
 int main() {
     // Please write your code here.
     int arr[3][3] = {}, arr2[3] = {}, cnt = 0;
-
-    for (int i = 0; i < 3; i++) cin >> arr2[i];
+    char arr5[3][3] = {};
 
     for (int i = 0; i < 3; i++) {
-        int x = 100;
         for (int j = 0; j < 3; j++) {
-            
-            arr[i][j] = arr2[i] / x;
-            arr2[i] %= x;
-            x /= 10;
-            
-            
-            
+            cin >> arr5[i][j];
+            arr[i][j] = arr5[i][j] - '0';
         }
-    
-
     }
 
-    for (int k = 1; k <= 3; k++) {
+    for (int i = 1; i < 9; i++) {
+        for (int j = i + 1; j <= 9; j++) {
+            bool flag = false;
+            for (int k = 1; k <= 3; k++) {
+                
         
-        if (k == 1) {
-            for (int l = 0; l < 3; l++) {
+                if (k == 1 && !flag) {
+                    for (int l = 0; l < 3; l++) {
                 
-                for (int m = 0; m < 3; m++) {
-                    arr2[m] = arr[l][m];
+                        for (int m = 0; m < 3; m++) {
+                            arr2[m] = arr[l][m];
+                        }
+                        sort(arr2, arr2 + 3);
+                        if (arr2[0] == i && arr2[2] == j && (arr2[1] == i || arr2[1] == j)) {
+                            cnt++;
+                            
+                            flag = true;
+                            break;
+                        }
+
+                    }
                 }
-                sort(arr2, arr2 + 3);
-                if (arr2[0] != arr2[2] && (arr2[0] == arr2[1] || arr2[1] == arr2[2])) cnt++;
+                else if (k == 2 && !flag) {
+                    for (int l = 0; l < 3; l++) {
+                
+                        for (int m = 0; m < 3; m++) {
+                            arr2[m] = arr[m][l];
+                        }
+                        sort(arr2, arr2 + 3);
+                        if (arr2[0] == i && arr2[2] == j && (arr2[1] == i || arr2[1] == j)) {
+                            cnt++;
+                            
+                            flag = true;
+                            break;
+                        }
+
+                    }
+                }
+                else if (k == 3 && !flag) {
+                    int arr3[3] = {arr[0][0], arr[1][1], arr[2][2]};
+                    sort(arr3, arr3 + 3);
+                    if (arr3[0] == i && arr3[2] == j && (arr3[1] == i || arr3[1] == j)) {
+                        cnt++;
+                        
+                        flag = true;
+                        break;
+                    }
+
+                    int arr4[3] = {arr[0][2], arr[1][1], arr[2][0]};
+                    sort(arr4, arr4 + 3);
+                    if (arr4[0] == i && arr4[2] == j && (arr4[1] == i || arr4[1] == j)) {
+                        cnt++;
+                        
+                        flag = true;
+                        break;
+                    }
+
+                }
+                if (flag) break;
+
 
             }
         }
-        else if (k == 2) {
-            for (int l = 0; l < 3; l++) {
-                
-                for (int m = 0; m < 3; m++) {
-                    arr2[m] = arr[m][l];
-                }
-                sort(arr2, arr2 + 3);
-                if (arr2[0] != arr2[2] && (arr2[0] == arr2[1] || arr2[1] == arr2[2])) cnt++;
-
-            }
-        }
-        else {
-            int arr3[3] = {arr[0][0], arr[1][1], arr[2][2]};
-            sort(arr3, arr3 + 3);
-            if (arr3[0] != arr3[2] && (arr3[0] == arr3[1] || arr3[1] == arr3[2])) cnt++;
-            int arr4[3] = {arr[0][2], arr[1][1], arr[2][0]};
-            sort(arr4, arr4 + 3);
-            if (arr4[0] != arr4[2] && (arr4[0] == arr4[1] || arr4[1] == arr4[2])) cnt++;
-
-        }
-
-
     }
+
+
+    
 
     cout << cnt;
 
