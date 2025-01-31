a4e10279ac5e8abc45bff2a937b6b80b888bf283
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-31 21:17:08 +0900

    Title: 전부 겹치는 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250131/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 250131/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -5,6 +5,8 @@ int main() {
     // Please write your code here.
     int n, arr[100][2] = {}, cnt[101] = {};
 
+    cin >> n;
+
     for (int i = 0; i < n; i++) {
         cin >> arr[i][0] >> arr[i][1];
         for (int j = arr[i][0]; j <= arr[i][1]; j++) cnt[j]++;
@@ -12,6 +14,7 @@ int main() {
 
     for (int i = 0; i < 101; i++) {
         if (cnt[i] == n) {
+            
             cout << "Yes";
             return 0;
         }
