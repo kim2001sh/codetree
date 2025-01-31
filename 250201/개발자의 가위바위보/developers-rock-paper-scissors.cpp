c9b7f06a7480801e4b481b5097f821c386dd58fe
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-01 02:05:16 +0900

    Title: 개발자의 가위바위보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/개발자의 가위바위보/developers-rock-paper-scissors.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[100][2] = {}, max_val = 0, cnt1, cnt2;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) cin >> arr[i][0] >> arr[i][1];
+
+
+    cnt1 = 0;
+    cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if ((arr[i][0] + arr[i][1]) % 3 == 1) cnt1++;
+        else if ((arr[i][0] + arr[i][1]) % 3 == 2) cnt2++;
+    }
+
+    max_val = max(cnt1, cnt2);
+
+    cout << max_val;
+
+    return 0;
+}
