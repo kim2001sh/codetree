6ae22a24996f70033a6e52db3eefa919b94c711b
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-01 02:52:40 +0900

    Title: ABC 줄 세우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250201/ABC 줄 세우기/abc-line-up.cpp
+++ 250201/ABC 줄 세우기/abc-line-up.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
     // Please write your code here.
-    int arr[26] = {}, n, cnt = 0;
+    int arr[26] = {}, n, cnt = 0, arr3[26] = {};
     char arr2[26] = {};
 
     cin >> n;
@@ -11,12 +12,16 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr2[i];
         arr[i] = arr2[i] - 65;
+        arr3[i] = arr[i];
 
     }
 
+    sort(arr3, arr3 + n);
+    
+
     while (1) {
         for (int i = 0; i < n; i++) {
-            if (arr[i] != i) break;
+            if (arr[i] != arr3[i]) break;
             if (i == n - 1) {
                 cout << cnt;
                 return 0;
@@ -25,10 +30,10 @@ int main() {
         bool flag = false;
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arr[i] == j && arr[j] == i) {
+                if (arr[i] == arr3[j] && arr[j] == arr3[i]) {
                     cnt++;
-                    arr[i] = i;
-                    arr[j] = j;
+                    arr[i] = arr3[i];
+                    arr[j] = arr3[j];
                     flag = true;
                 }
             }
@@ -36,7 +41,7 @@ int main() {
         if (flag == false) {
             int cnt2 = 0;
             for (int i = 0; i < n; i++) {
-                if (arr[i] != i) cnt2++;
+                if (arr[i] != arr3[i]) cnt2++;
             }
             if (cnt2 == 0) {
                 cout << cnt;
