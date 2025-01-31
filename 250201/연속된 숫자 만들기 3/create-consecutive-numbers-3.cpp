8bd5cdf8ab58e8b736c3fbfc9eb336f40c059f69
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-01 02:26:43 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250201/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 250201/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -10,16 +10,18 @@ int main() {
     for (int i = 0; i < 3; i++) cin >> arr[i];
 
     while (1) {
-        if (arr[2] - arr[1] == 1 && arr[1] - arr[0] == 1) break;
+        if (arr[2] - arr[1] == 1 && arr[1] - arr[0] == 1) {
+            cnt = 0;
+            break;
+        }
+        
         else if (arr[2] - arr[1] <= arr[1] - arr[0]) {
-            cnt++;
-            arr[2] = arr[1] - 1;
-            sort(arr, arr + 3);
+            cnt = arr[1] - arr[0] - 1;
+            break;
         }
         else if (arr[2] - arr[1] > arr[1] - arr[0]) {
-            cnt++;
-            arr[0] = arr[1] + 1;
-            sort(arr, arr + 3);
+            cnt = arr[2] - arr[1] - 1;
+            break;
         }
     }
 
