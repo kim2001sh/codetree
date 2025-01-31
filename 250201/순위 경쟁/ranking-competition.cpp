c03336636c2f16ba651386554c3e037a2111ea47
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-01 04:01:10 +0900

    Title: 순위 경쟁, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250201/순위 경쟁/ranking-competition.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, arr[100] = {}, a = 1, b = 1, c = 1, scorea = 0, scoreb = 0, scorec = 0, cnt = 0;
+    char arr2[100] = {};
+    cin >> n;
+
+    for (int i = 0; i < n; i++) cin >> arr2[i] >> arr[i];
+
+    for (int i = 0; i < n; i++) {
+        if (arr2[i] == 'A') scorea += arr[i];
+        else if (arr2[i] == 'B') scoreb += arr[i];
+        else scorec += arr[i];
+
+        if (scorea > scoreb && scorea > scorec && a == 1 && b == 0 && c == 0) ;
+        else if (scorea < scoreb && scorec < scoreb && a == 0 && b == 1 && c == 0) ;
+        else if (scorea < scorec && scoreb < scorec && a == 0 && b == 0 && c == 1) ;
+        else if (scorea == scoreb && scorea == scorec && a == 1 && b == 1 && c == 1) ;
+        else if (scorea < scoreb && scoreb == scorec && a == 0 && b == 1 && c == 1) ;
+        else if (scoreb < scorec && scorec == scorea && a == 1 && b == 0 && c == 1) ;
+        else if (scorec < scorea && scoreb == scorea && a == 1 && b == 1 && c == 0) ;
+        else {
+            cnt++;
+            if (scorea == scoreb == scorec) {
+                a = 1;
+                b = 1;
+                c = 1;
+            }
+            else if (scorea > scoreb && scorea > scorec) {
+                a = 1;
+                b = 0;
+                c = 0;
+            }
+            else if (scorea < scoreb && scorec < scoreb) {
+                a = 0;
+                b = 1;
+                c = 0;
+            }
+            else if (scorea < scorec && scoreb < scorec) {
+                a = 0;
+                b = 0;
+                c = 1;
+            }
+            else if (scorea < scoreb && scoreb == scorec) {
+                a = 0;
+                b = 1;
+                c = 1;
+            }
+            else if (scoreb < scorec && scorec == scorea) {
+                a = 1;
+                b = 0;
+                c = 1;
+            }
+            else if (scorec < scorea && scoreb == scorea) {
+                a = 1;
+                b = 1;
+                c = 0;
+            }
+            
+        }
+        
+    }
+
+    cout << cnt;
+
+    return 0;
+}
