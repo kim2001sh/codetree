f7299745096c17c837394842197c5fb6969808fb
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-01 01:23:05 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250201/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[3] = {};
+
+    for (int i = 0; i < 3; i++) cin >> arr[i];
+
+    sort(arr, arr + 3);
+
+    if (arr[1] - arr[0] == 1 && arr[2] - arr[1] == 1) cout << 0;
+    else if (arr[1] - arr[0] == 1 || arr[2] - arr[1] == 1) cout << 2;
+    else if (arr[1] - arr[0] == 2 || arr[2] - arr[1] == 2) cout << 1;
+    else cout << 2;
+
+
+    return 0;
+}
