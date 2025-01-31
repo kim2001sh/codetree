ff9b4e1c9f45496da40693948a2f0cdb2d2df1f7
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-01-31 20:08:41 +0900

    Title: 이상한 폭탄 3, Time: 9ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 250131/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, k, arr[100] = {}, bomb[1000001] = {}, max_val = 0;
+
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    for (int i = 0; i < n; i++) { // 탐색
+        for (int l = 1; l <= k; l++) {
+            if (i + k < 100 && arr[i] == arr[i + k]) {
+                bomb[arr[i]]++;
+                break;
+            }
+            else if (i - k >= 0 && arr[i] == arr[i - k]) {
+                bomb[arr[i]]++;
+                break;
+            }
+        }
+    }
+
+    cout << *max_element(bomb, bomb + 1000001);
+    return 0;
+}
