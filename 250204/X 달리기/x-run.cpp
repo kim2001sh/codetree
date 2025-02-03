3b7e21081b1c7bb35e106c6a3392d0e4599af2fd
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-04 07:55:26 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250204/X 달리기/x-run.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x;
+    
+    cin >> x;
+
+    cout << 2 * sqrt(x - 1);
+    return 0;
+}
