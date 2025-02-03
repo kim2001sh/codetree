6f82134b58a8cd8d9278919b6bbaef5d166738e8
kim2001sh <151429245+kim2001sh@users.noreply.github.com>
2025-02-04 07:52:04 +0900

    Title: 홀수 짝수의 묶음, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/홀수 짝수의 묶음/odd-even-bundle.cpp
+++ 250204/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -3,25 +3,68 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int n, arr[1000] = {}, cnt = 0, sum = 0;
-
+    int n, even_cnt = 0, odd_cnt, arr[1000] = {}, cnt = 0;
     bool even = true;
 
     cin >> n;
 
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
     for (int i = 0; i < n; i++) {
-        sum += arr[i];
-        if (even && sum % 2 == 0 || !even && sum % 2 == 1) {
-            cnt++;
-            sum = 0;
-            if (even) even = false;
-            else even = true;
-        }
-        else if (i == n - 1) cnt--;
+        cin >> arr[i];
+        if (arr[i] % 2 == 0) even_cnt++;
     }
 
-    cout << min(0, cnt);
+    odd_cnt = n - even_cnt;
+
+
+    while (odd_cnt != 0 || even_cnt != 0) {
+        if (even) {
+            if (even_cnt > odd_cnt) {
+                even_cnt = odd_cnt;
+                cnt++;
+                even = false;
+            }
+            else if (even_cnt == odd_cnt) {
+                even_cnt -= 1;
+                cnt++;
+                even = false;
+            }
+            else {
+                if (odd_cnt >= 2) {
+                    cnt++;
+                    odd_cnt -= 2;
+                    even = false;
+                }
+                else {
+                    cnt--;
+                    break;
+                }
+
+            }
+        }
+        else {
+            if (even_cnt > odd_cnt) {
+                even_cnt = odd_cnt;
+                odd_cnt -= 1;
+                cnt++;
+                even = true;
+            }
+            else if (even_cnt == odd_cnt) {
+                odd_cnt -= 1;
+                cnt++;
+                even = true;
+            }
+            else {
+                odd_cnt -= 1;
+                cnt++;
+                even = true;
+            }
+
+        }
+    }
+
+    cout << cnt;
+    
+
+
     return 0;
 }
